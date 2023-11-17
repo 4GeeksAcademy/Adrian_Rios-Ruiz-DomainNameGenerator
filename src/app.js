1e1962d8b7b70e15283ea6190df964233799fa92
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-11-17 14:26:36 +0000

    Primera prueba

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let part1 = ["the", "our", "this", "them"];
+  let part2 = ["great", "big", "beautiful", "amazing"];
+  let part3 = ["jogger", "racoon", "bikkers", "sniper"];
+  let part4 = [".com", ".is", ".our", ".our"];
+
+  function randomIndex(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+  let domain =
+    randomIndex(part1) +
+    randomIndex(part2) +
+    randomIndex(part3) +
+    randomIndex(part4);
+
+  document.querySelector("#domainText").innerHTML = domain;
+  console.log(domain);
 };
