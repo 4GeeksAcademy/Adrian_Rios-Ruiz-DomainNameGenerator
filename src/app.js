8caf5ebc611232a86a1552dcc189750b8372c3fc
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-11-20 20:08:32 +0000

    Trabajo realizado

--- src/app.js
+++ src/app.js
@@ -11,14 +11,34 @@ window.onload = function() {
   let noun = ["jogger", "racoon"];
   let end = [".com", ".is"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let x = 0; x < adj.length; x++) {
-      for (let y = 0; y < noun.length; y++) {
-        for (let z = 0; z < end.length; z++) {
-          let domain = pronoun[i] + adj[x] + noun[y] + end[z];
-          console.log(domain);
+  function domaingenerator(pronoun, adj, noun, end) {
+    // Inicializar el resultado como una lista vacía
+    let domains = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let x = 0; x < adj.length; x++) {
+        for (let y = 0; y < noun.length; y++) {
+          for (let z = 0; z < end.length; z++) {
+            // Crear el dominio y agregarlo a la lista
+            let domain = pronoun[i] + adj[x] + noun[y] + end[z];
+            domains.push(domain);
+          }
         }
       }
     }
+
+    // Devolver la lista de dominios
+    return domains;
   }
+
+  // Llamar a la función y almacenar el resultado en la variable result
+  let result = domaingenerator(pronoun, adj, noun, end);
+
+  // Imprimir los resultados en la consola
+  console.log(result);
+
+  // Mostrar los resultados en el HTML
+  document.getElementById("domainResult").innerHTML = result
+    .map(domain => '<li class="list-group-item">' + domain + "</li> ")
+    .join("");
 };
